33c9a0bc3ee23d029d03ce4a9c74aac0ff092790
Rye Terrell <ryeterrell@ryeterrell.net>
2015-10-09 12:38:27 -0500

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 rm -rf inspector-gh-temp*
 wget -q "https://github.com/zenoss/inspector/archive/master.zip" -O inspector-gh-temp.zip
-unzip inspector-gh-temp.zip -d inspector-gh-temp
+unzip -qq inspector-gh-temp.zip -d inspector-gh-temp
 inspector-gh-temp/inspector-master/inspect
+rm -rf inspector-gh-temp*
