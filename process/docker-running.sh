572c3cabb01ff63a7501acc669fbd89253c24c99
ryeterrell <ryeterrell@ryeterrell.net>
2015-10-09 21:13:46 -0500

    add docker-running processor script

---
+++ process/docker-running.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# zenoss-inspector-deps ps-aux.sh
+
+grep "docker -d" ps-aux.sh.stdout &>/dev/null
+
+if [ $? -ne 0 ]
+    then
+        grep "docker daemon" ps-aux.sh.stdout &>/dev/null
+        if [ $? -ne 0 ]
+            then echo "Docker doesn't appear to be running."
+        fi
+fi
