066e3494438fee7b424ee0990b43368ed4270782
Glenn Jones <gjones@zenoss.com>
2016-02-08 11:07:43 -0600

    Make sure unzip is installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+# Make sure unzip is installed
+UNZIP_INSTALLED=`rpm -qa unzip`
+if [ -z "$UNZIP_INSTALLED" ]; then
+	echo "Installing unzip ..."
+	yum install -y unzip
+fi
+
+# clear out any previous results
 rm -rf inspector-gh-temp*
+
 wget -q "https://github.com/zenoss/inspector/archive/master.zip" -O inspector-gh-temp.zip
 unzip -qq inspector-gh-temp.zip -d inspector-gh-temp
 inspector-gh-temp/inspector-master/inspect
