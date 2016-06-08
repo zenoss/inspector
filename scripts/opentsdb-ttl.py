1141568ccae93c9d28e106971cc3fb9f9a873e13
Matthew Parlette <mparlette@zenoss.com>
2016-06-08 16:17:38 -0400

    Added opentsdb-ttl.py info script.
    This checks each tsdb database in hbase to check the TTL.
    
    If the TTL is `FOREVER` or `2147483647`, a message is shown to run the command to properly set the TTL to 90 days.
    
    Test for ZEN-22981.

---
+++ scripts/opentsdb-ttl.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# zenoss-inspector-info
+# zenoss-inspector-tags rm opentsdb ZEN-22981
+
+import subprocess as sp
+
+
+def run(cmd):
+    p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        raise sp.CalledProcessError(p.returncode, ' '.join(cmd))
+    return stdout, stderr
+
+
+def main():
+    cmd = ["serviced", "service", "shell", "writer",
+           "bash", "-c", "echo list|/opt/hbase/bin/hbase shell"]
+    stdout, stderr = run(cmd)
+
+    # Get the tsdb table names for all tenants
+    for line in stdout.strip().split("\n"):
+        if line.endswith("-tsdb"):
+            cmd = ["serviced", "service", "shell", "writer",
+                   "bash", "-c",
+                   "echo 'describe \"{}\"'|/opt/hbase/bin/hbase shell".format(
+                       line)]
+            stdout, stderr = run(cmd)
+            if ("TTL => 'FOREVER'" in stdout or
+                    "TTL => '2147483647'" in stdout):
+                print "Tenant {} has opentsdb TTL set to FOREVER".format(
+                    line)
+                print "Set to 90 days with:"
+                print "    serviced service shell writer " \
+                    "/opt/opentsdb/set-opentsdb-table-ttl.sh 7776000"
+
+if __name__ == "__main__":
+    main()
