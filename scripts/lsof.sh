e4ac74e4bf8cd554990e6f581f828248b35fdcba
Glenn Jones <gjones@zenoss.com>
2016-06-29 17:54:35 -0500

    Print explanations for OS scripts which often fail due to missing packages

--- scripts/lsof.sh
+++ scripts/lsof.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# zenoss-inspector-info
 # zenoss-inspector-tags slow os
+
+command -v lsof >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+   echo "lsof is not installed."
+   exit 0;
+fi
 
 lsof
