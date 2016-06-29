cd80ea93bcb3ba549902857ec4010f41d7604ead
Glenn Jones <gjones@zenoss.com>
2016-06-29 18:15:06 -0500

    Correct changes for missing executables

--- scripts/lsb-release-a.sh
+++ scripts/lsb-release-a.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# zenoss-inspector-info
 # zenoss-inspector-tags os
 
 command -v lsb_release >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-   echo "lsb_release is not installed; distribution details not available."
-   exit 0;
+   echo "lsb_release is not installed." 1>&2
+   exit 1;
 fi
 
 lsb_release -a
--- scripts/lsof.sh
+++ scripts/lsof.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# zenoss-inspector-info
 # zenoss-inspector-tags slow os
 
 command -v lsof >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-   echo "lsof is not installed."
-   exit 0;
+   echo "lsof is not installed." 1>&2
+   exit 1;
 fi
 
 lsof
