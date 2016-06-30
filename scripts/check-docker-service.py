b32cc23658974f548cc1fa8205a0d011bd35a591
Bill Loss <closs@zenoss.com>
2016-06-30 16:49:58 -0500

    Adding check in docker config for TimeoutSec, this should be set to 300

--- scripts/check-docker-service.py
+++ scripts/check-docker-service.py
@@ -10,6 +10,10 @@ def bail():
     print "It doesn't appear /etc/sysconfig/docker has been referenced in your docker.service file."
     import sys; sys.exit()
 
+def bail2():
+    print 'Docker configuration should specify "TimeoutSec=300" please adjust in "/etc/systemd/system/docker.service.d/docker.conf", prior to CC 1.1.3 this should be set in /usr/lib/systemd/system/docker.service'
+    import sys; sys.exit()
+
 def main():
     config = ConfigParser.RawConfigParser()
     config.read('docker-service.sh.stdout')
@@ -17,6 +21,11 @@ def main():
         bail()
     if config.get('Service', 'EnvironmentFile') != '-/etc/sysconfig/docker':
         bail()
+    if not config.has_option('Service', 'TimeoutSec'):
+        bail2()
+    if config.get('Service', 'TimeoutSec') != '300':
+        bail2()
+
 
 if __name__ == "__main__":
     main()
