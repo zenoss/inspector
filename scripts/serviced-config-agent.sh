f179433fe4fb9dd786bda2aa273c7abffb023f7a
Matthew Parlette <mparlette@zenoss.com>
2016-07-07 16:05:48 -0400

    Support all values for SERVICED_MASTER and SERVICED_AGENT
    Specifically, those settings can be:
    
    * 0/1
    * true/false
    * t/f
    * y/n
    
    Finishes INSP-1

--- scripts/serviced-config-agent.sh
+++ scripts/serviced-config-agent.sh
@@ -6,11 +6,11 @@
 
 # Verify that if this is a master node, that it is also an agent
 
-grep "SERVICED_MASTER=1" serviced-config.sh.stdout &>/dev/null
+grep -E -i 'SERVICED_MASTER=(1|true|t|yes)' serviced-config.sh.stdout &>/dev/null
 
 if [ $? -eq 0 ]
     then
-        grep "SERVICED_AGENT=1" serviced-config.sh.stdout &>/dev/null
+        grep -E -i "SERVICED_AGENT=(1|true|t|yes)" serviced-config.sh.stdout &>/dev/null
         if [ $? -ne 0 ]
             then echo "CHECK FAILED: SERVICED_AGENT must be enabled on the Master for serviced commands to work"
         fi
