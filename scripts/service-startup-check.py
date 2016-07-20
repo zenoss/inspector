014c577030ffdf5e53be0f75f42f470e62625ea6
Bill Loss <closs@zenoss.com>
2016-07-20 17:53:53 -0500

    Changing to use json to grab service name

--- scripts/service-startup-check.py
+++ scripts/service-startup-check.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 # zenoss-inspector-info
-# zenoss-inspector-tags os verify
+# zenoss-inspector-tags verify
 # zenoss-inspector-deps serviced-service-list-v.sh
+import json
+from pprint import pprint
 
 def main():
-    with open("serviced-service-list-v.sh.stdout", 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        if "Startup" and "/opt/zenoss/ZenPacks" in line:
-            print 'There is a problem with the Startup line for the following:'
-            print line
-            print 'To correct this edit the service definition with "serviced service edit SERVICE_NAME"'
-            print 'and edit the Startup line to use the symbolic link at "/opt/zenoss/bin"'
+    with open('serviced-service-list-v.sh.stdout') as data_file:
+        data = json.load(data_file)
+        #pprint(data)
+        for i in data:
+            if "/opt/zenoss/ZenPacks" in i["Startup"]:
+                print "There is an issue with Startup command %s" % i["Name"]
+                print i["Startup"]
+                print 'To correct this you should edit the service definition',\
+                      'with "serviced service edit %s" changing the Startup' % i["Name"], \
+                      'line to use the symbolic link at "/opt/zenoss/bin/%s"' % i["Name"]
+
 if __name__ == "__main__":
     main()
