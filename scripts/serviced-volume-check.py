143ca8a9bbb998cc1207c8d2ca512032a4626aab
Bill Loss <closs@zenoss.com>
2016-07-27 15:06:33 -0500

    Check for storage mismatch, work in progress at this point but should help show if a problem exists

--- scripts/serviced-volume-check.py
+++ scripts/serviced-volume-check.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 # zenoss-inspector-info
-# zenoss-inspector-tags verify
+# zenoss-inspector-tags os verify
 # zenoss-inspector-deps serviced-storage.sh storage-config.sh
-
+import re
 
 def main():
     with open("serviced-storage.sh.stdout", 'r') as f:
@@ -11,14 +11,17 @@ def main():
     for line in lines:
         if "Application Data" in line:
             volId = line.split()[0]
-            with open("/opt/serviced/var/volumes/.devicemapper/volumes/$volId/metadata.json")
+            path = "/opt/serviced/var/volumes/.devicemapper/volumes/" + volId + "/metadata.json"
+            with open(path, 'r') as f2:
+                line = f2.readline()
+                pattern = re.compile('^\{\"CurrentDevice\"\:\"(?P<blkId>\w*)')
+                pool = pattern.match(line).group('blkId')
 
-
-    with open("storage-config.sh.stdout", 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        if "/opt/serviced/var/volumes/" in line:
-            print "Storage volume mismatch see lsblk output for further information"
+            with open("storage-config.sh.stdout", 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if "opt/serviced/var/volumes" in line:
+                        print "Storage volume mismatch on serviced-serviced--pool: %s see lsblk output for more information CC-2487" % pool
 
 
 if __name__ == "__main__":
