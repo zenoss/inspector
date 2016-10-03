3f03287b0f0a80bc0dd2aa0b7bd292f4697a81b1
Bill Loss <closs@zenoss.com>
2016-10-03 13:23:15 -0500

    Fix to this script to avoid false positives and get more accurate results

--- scripts/serviced-volume-check.py
+++ scripts/serviced-volume-check.py
@@ -1,28 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                                                                                                                                       
 
-# zenoss-inspector-info
-# zenoss-inspector-tags os verify
-# zenoss-inspector-deps serviced-storage.sh storage-config.sh
+# zenoss-inspector-info                                                                                                                                     
+# zenoss-inspector-tags os verify                                                                                                                           
+# zenoss-inspector-deps serviced-storage.sh storage-config.sh                                                                                               
 import re
 
 def main():
-    with open("serviced-storage.sh.stdout", 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        if "Application Data" in line:
-            volId = line.split()[0]
-            path = "/opt/serviced/var/volumes/.devicemapper/volumes/" + volId + "/metadata.json"
-            with open(path, 'r') as f2:
-                line = f2.readline()
-                pattern = re.compile('^\{\"CurrentDevice\"\:\"(?P<blkId>\w*)')
-                pool = pattern.match(line).group('blkId')
+    with open("serviced-storage.sh.stdout", 'r') as f1:
+        for line1 in f1.readlines():
+            if "Application Data" in line1:
+                volId = line1.split()[0]
+                path = "/opt/serviced/var/volumes/.devicemapper/volumes/" + volId + "/metadata.json"
+                with open(path, 'r') as f2:
+                    line2 = f2.readline()
+                    pattern1 = re.compile('^\{\"CurrentDevice\"\:\"(?P<blkId>\w*)')
+                    pool = pattern1.match(line2).group('blkId')
 
-            with open("storage-config.sh.stdout", 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if "opt/serviced/var/volumes" in line:
-                        print "Storage volume mismatch on serviced-serviced--pool: %s see lsblk output for more information CC-2487" % pool
-
+                with open("storage-config.sh.stdout", 'r') as f3:
+                    for line3 in f3.readlines():
+                        line3 = ''.join(i for i in line3 if ord(i)<128)
+                        if "lv-display" in line3:
+                            return
+                        if "/exports/serviced_volumes_v2/" in line3:
+                            pattern2 = re.compile('.*:\d+(-\d+)-(?P<poolId>\w*)\s.*')
+                            upool = pattern2.match(line3).group('poolId')
+                            if upool != pool:
+                                print "Storage volume mismatch on serviced-serviced--pool: %s see lsblk output for more information CC-2487" % pool
 
 if __name__ == "__main__":
     main()
