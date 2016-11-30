59c75116b47e2829d406d84c9285533d7a8fa9b0
Alex Chow <achow@zenoss.com>
2016-11-30 15:04:02 -0600

    Make it works for CC 1.0, 1.1, and 1.2

--- scripts/nfs-server-service.sh
+++ scripts/nfs-server-service.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# RM installation doc recommends to use nfs-server drop-in conf files
-if [ -f /etc/systemd/system/nfs-server.service.d/nfs-server.conf ]; then
-    cat /etc/systemd/system/nfs-server.service.d/nfs-server.conf
+# RM installation doc 1.2 recommends to use nfs-server drop-in conf files
+
+CC_VERSION=`grep -w 'Version:' serviced-version.sh.stdout 2>/dev/null | awk '{print $2}' | cut -d\. -f1-2`
+
+if [[ $CC_VERSION == "1.0" || $CC_VERSION == "1.1" ]]
+then
+    [[ -f /lib/systemd/system/nfs-server.service ]] && cat /lib/systemd/system/nfs-server.service
+elif [[ $CC_VERSION == "1.2" ]]
+then
+    [[ -f /etc/systemd/system/nfs-server.service.d/nfs-server.conf ]] && cat /etc/systemd/system/nfs-server.service.d/nfs-server.conf
 fi
