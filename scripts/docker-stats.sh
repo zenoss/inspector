083207cbe6d22be8f6fed504853b719503aecbbf
Ron Phillips <rphillips@zenoss.com>
2017-04-06 09:40:00 -0500

    Error out of docker-stats if not running

--- scripts/docker-stats.sh
+++ scripts/docker-stats.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # zenoss-inspector-tags docker
+# zenoss-inspector-deps ps-aux.sh
+
+egrep "(docker -d|docker daemon|dockerd)" ps-aux.sh.stdout &>/dev/null
+
+if [ $? -ne 0 ]; then
+    (>&2 echo "Docker doesn't appear to be running.")
+    exit 1
+fi
 
 docker ps -q | xargs docker stats --no-stream
 
