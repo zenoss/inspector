4e6d6c1ef3e0d290d54704636162cc9ffc3bd5fc
Supriya Ambati <svambati@gmail.com>
2017-06-19 10:26:58 -0500

    INSP-88: Addressed review

--- scripts/get-serviced-audit-log.sh
+++ scripts/get-serviced-audit-log.sh
@@ -3,9 +3,8 @@
 
 # Control Center audit log
 
-if [[ -f /var/log/serviced/serviced-audit.log ]]; then
-	sudo cat /var/log/serviced/serviced-audit.log
+if [ -f /var/log/serviced/serviced-audit.log ]; then
+	cat /var/log/serviced/serviced-audit.log
 else
 	echo "serviced audit log doesn't exist" 1>&2
-	exit 1
 fi
--- scripts/get-zenoss-audit-log.sh
+++ scripts/get-zenoss-audit-log.sh
@@ -3,8 +3,7 @@
 # Resource Manager audit log
 
 if [ -f /var/log/serviced/application-audit.log ]; then
-	sudo cat /var/log/serviced/application-audit.log
+	cat /var/log/serviced/application-audit.log
 else
 	echo "Zenoss audit log doesn't exist" 1>&2
-	exit 1
 fi
