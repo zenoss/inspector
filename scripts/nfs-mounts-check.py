660ed93ac138faf8929ee911b9976ae05e141b75
Paul Johnston <pjohnston@zenoss.com>
2018-02-08 14:26:44 -0600

    INSP-97: Fixed ability for commented out default to be left in config file and added some detailed version reporting in the error message

--- scripts/nfs-mounts-check.py
+++ scripts/nfs-mounts-check.py
@@ -5,13 +5,25 @@
 # zenoss-inspector-deps nfs-mounts.sh
 
 def main():
+
+    DefaultVersion = ""
+    ActiveVersion = ""
+
     with open("nfs-mounts.sh.stdout", 'r') as f:
         for line in f.readlines():
-            if "Defaultvers" in line:
-                if "Defaultvers=4.0" not in line:
+            if "Defaultvers=" not in line:
+                continue
+            line = line.strip()
+            version = line.split("Defaultvers=",1)[1]
+            if line.startswith("#"): 
+                DefaultVersion = version
+            else:
+                ActiveVersion = version
+
+    if "4.0" not in DefaultVersion and "4.0" not in ActiveVersion:
                     print "NFS mounts should force the specific version: 4.0."
+                    print "Default version is set to '%s' and override version is set to '%s'" % (DefaultVersion, ActiveVersion)
                     print "See: https://support.zenoss.com/hc/en-us/articles/115005085763-Potential-issues-running-with-RHEL-7-4-or-CentOS-7-4"
-                    
     return
 
 if __name__ == "__main__":
